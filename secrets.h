8acaaab16f02f9862ca7a2b32696be68b465ecd1
Pareidolial Jebus <53192108+pareidolialjebus@users.noreply.github.com>
2019-09-25 23:32:46 +0100

    Create secrets.h

---
+++ secrets.h
@@ -0,0 +1,15 @@
+// Update this file with your credentials
+
+// WiFi
+// --------------------------------------------------------------------------------
+const char* ssid = "your_wifi_ssid";
+const char* password = "your_wifi_password"; //
+
+// MQTT
+// --------------------------------------------------------------------------------
+const char* mqtt_server = "your.mqtt.broker";
+String mqtt_user = "your_mqtt_user";
+String mqtt_pass = "your_mqtt_password";
+const char* mqtt_topic = "outwardMQTTtopic";
+String clientId = "id_of_your_choosing";
+String mqtt_in = "inwardMQTTtopic";
