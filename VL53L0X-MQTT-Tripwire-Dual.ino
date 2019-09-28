e99942e3bd5ba48ecfa11fcdfb380cd117d0f04d
Pareidolial Jebus <53192108+pareidolialjebus@users.noreply.github.com>
2019-09-28 17:03:43 +0100

    Update VL53L0X-MQTT-Tripwire-Dual.ino
    Update to allow setting of laser 1 & 2 min / max sensing range, calibration modes controlled over MQTT

--- VL53L0X-MQTT-Tripwire-Dual.ino
+++ VL53L0X-MQTT-Tripwire-Dual.ino
@@ -1,4 +1,4 @@
-// V0.4
+// V0.5
 // Dual VL53L0x Time of Flight Laser Rangefinders as directional tripwire
 // Reports "on1" and "on2" as MQTT payload when beams broken by a person
 // or objects travelling as long as they break both beams sequentially.
@@ -51,6 +51,13 @@ int oldDirection = 0;
 int dirReset = 0;
 int mqMsg = 0;
 
+// Calibration and range
+int calibration_mode = 0;
+long v1_max = 2000;
+long v1_min = 0;
+long v2_max = 2000;
+long v2_min = 0;
+
 // reset L0x1 L0x2 and assign thier I2C addresses
 // then boot each
 // --------------------------------------------------------------------------------
@@ -91,53 +98,97 @@ void read_dual_sensors() {
   lox1.rangingTest(&measure1, false);
   lox2.rangingTest(&measure2, false);
 
-  // L0x1 triggered?
-  if (measure1.RangeStatus != 4) {
-    Timer1 = 10;
-  }
-  Timer1 = Timer1 - 1;
-  if (Timer1 < 0) {
-    Timer1 = 0;
-  }
-
-  // L0x2 triggered?
-  if (measure2.RangeStatus != 4) {
-    Timer2 = 10;
-  }
-  Timer2 = Timer2 - 1;
-  if (Timer2 < 0) {
-    Timer2 = 0;
-  }
-
-  // If both triggered, which direction?
-  if ((Timer1 != 0) && (Timer2 != 0)) {
-    if (Timer1 > Timer2) {
-      newDirection = 1;
-    }
-    if (Timer1 < Timer2) {
-      newDirection = 2;
+  if (calibration_mode == 1) {
+    if (measure1.RangeStatus != 4) {
+      v1_max = measure1.RangeMilliMeter;
+      Serial.println(v1_max);
+    } else {
+      Serial.println(v1_max);
     }
   }
 
-  // If this direction is "new", start timer for "off"
-  if ((oldDirection != newDirection) && (newDirection != 0)) {
-    mqMsg = newDirection;
-    Timer3 = 10;
-    dirReset = 1;
-  }
-  if (Timer3 != 0) {
-    Timer3 = Timer3 - 1;
-    if (Timer3 < 0) {
-      Timer3 = 0;
+  if (calibration_mode == 2) {
+    if (measure2.RangeStatus != 4) {
+      v2_max = measure2.RangeMilliMeter;
+      Serial.println(v2_max);
+    } else {
+      Serial.println(v2_max);
     }
   }
-  if ((Timer3 == 0) && (dirReset == 1)) {
-    mqMsg = 3;
-    dirReset = 0;
-    oldDirection = 0;
-    newDirection = 0;
+
+  if (calibration_mode == 3) {
+    if (measure1.RangeStatus != 4) {
+      v1_min = measure1.RangeMilliMeter;
+      Serial.println(v1_min);
+    } else {
+      Serial.println(v1_min);
+    }
   }
-  oldDirection = newDirection;
+
+  if (calibration_mode == 4) {
+    if (measure2.RangeStatus != 4) {
+      v2_min = measure2.RangeMilliMeter;
+      Serial.println(v2_min);
+    } else {
+      Serial.println(v2_min);
+    }
+  }
+
+  // run mode
+  if (calibration_mode == 0)
+  {
+    // L0x1 triggered?
+    if (measure1.RangeStatus != 4) {
+      if ((measure1.RangeMilliMeter < v1_max) && (measure1.RangeMilliMeter > v1_min)){
+        Timer1 = 10;
+      }
+    }
+    Timer1 = Timer1 - 1;
+    if (Timer1 < 0) {
+      Timer1 = 0;
+    }
+
+    // L0x2 triggered?
+    if (measure2.RangeStatus != 4) {
+      if ((measure2.RangeMilliMeter < v2_max) && (measure2.RangeMilliMeter > v2_min)){
+        Timer2 = 10;
+      }
+    }
+    Timer2 = Timer2 - 1;
+    if (Timer2 < 0) {
+      Timer2 = 0;
+    }
+
+    // If both triggered, which direction?
+    if ((Timer1 != 0) && (Timer2 != 0)) {
+      if (Timer1 > Timer2) {
+        newDirection = 1;
+      }
+      if (Timer1 < Timer2) {
+        newDirection = 2;
+      }
+    }
+
+    // If this direction is "new", start timer for "off"
+    if ((oldDirection != newDirection) && (newDirection != 0)) {
+      mqMsg = newDirection;
+      Timer3 = 10;
+      dirReset = 1;
+    }
+    if (Timer3 != 0) {
+      Timer3 = Timer3 - 1;
+      if (Timer3 < 0) {
+        Timer3 = 0;
+      }
+    }
+    if ((Timer3 == 0) && (dirReset == 1)) {
+      mqMsg = 3;
+      dirReset = 0;
+      oldDirection = 0;
+      newDirection = 0;
+    }
+    oldDirection = newDirection;
+  }
 }
 
 
@@ -172,6 +223,37 @@ void callback(char* topic, byte* payload, unsigned int length) {
     Serial.print((char)payload[i]);
   }
   Serial.println();
+  if ((char)payload[0] == '1') {
+    calibration_mode = 1;
+    Serial.println("Set Laser 1 Max Range");
+  }
+  if ((char)payload[0] == '2') {
+    calibration_mode = 2;
+    Serial.println("Set Laser 2 Max Range");
+  }
+  if ((char)payload[0] == '3') {
+    calibration_mode = 3;
+    Serial.println("Set Laser 1 Min Range");
+  }
+  if ((char)payload[0] == '4') {
+    calibration_mode = 4;
+    Serial.println("Set Laser 2 Min Range");
+  }
+  if ((char)payload[0] == '0') {
+    calibration_mode = 0;
+    
+    Serial.println("Laser ranges set");
+    
+    Serial.print("L1 Max: ");
+    Serial.print(v1_max);
+    Serial.print(" Min: ");
+    Serial.print(v1_min);
+    
+    Serial.print("  L2 Max: ");
+    Serial.print(v2_max);
+    Serial.print(" Min: ");
+    Serial.println(v2_min);
+  }
 
 }
 
@@ -208,7 +290,7 @@ void setup() {
 
   // start MQTT outgoing
   Serial.println("Starting MQTT out");
-  client.setServer(mqtt_server, mqtt_port); 
+  client.setServer(mqtt_server, mqtt_port);
   // start MQTT incoming
   Serial.println("Starting MQTT in");
   client.setCallback(callback);
@@ -236,19 +318,19 @@ void loop() {
 
   if (mqMsg == 1) {
     client.publish(mqtt_topic, "on1");
-    Serial.print("MQTT:on1");
+    Serial.println("MQTT:on1");
     mqMsg = 0;
   }
 
   if (mqMsg == 2) {
     client.publish(mqtt_topic, "on2");
-    Serial.print("MQTT:on2");
+    Serial.println("MQTT:on2");
     mqMsg = 0;
   }
 
   if (mqMsg == 3) {
     client.publish(mqtt_topic, "off");
-    Serial.print("MQTT:off");
+    Serial.println("MQTT:off");
     mqMsg = 0;
   }
 
